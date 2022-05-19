2ad343e97502c361078355aa53db59a2b143e37a
holo228 <regina.gondarenko@mail.ru>
2022-05-19 15:38:10 +0300

    Тесты работают, требуется исправить регулярное выражение для проверки телефона

--- TestPrikol/BuyersTest.cs
+++ TestPrikol/BuyersTest.cs
@@ -12,6 +12,18 @@ namespace TestPrikol
         [Fact]
         public void Test_Post_Buyer()
             => Assert.True(_validation.IsValid(79208880519));
+
+        [Theory]
+        [InlineData(79208880519)]
+        [InlineData(79186162760)]
+        public void Test_Post_Buyer1(long? phoneNumber)
+            => Assert.True(_validation.IsValid(phoneNumber));
+
+        [Theory]
+        [InlineData(7920888051900000)]
+        [InlineData(09186162760)]
+        public void Test_Post_Buyer2(long? phoneNumber)
+            => Assert.False(_validation.IsValid(phoneNumber));
     }
 }
 
