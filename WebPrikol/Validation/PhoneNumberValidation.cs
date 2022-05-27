961a30048ba899fbbee4559d27b9b72e28881d9c
regin <regin@DESKTOP-5UULK23>
2022-05-27 22:34:22 +0300

    Улучшили регулярное выражение

--- WebPrikol/Validation/PhoneNumberValidation.cs
+++ WebPrikol/Validation/PhoneNumberValidation.cs
@@ -6,7 +6,7 @@ namespace WebPrikol.Validation
     {
         public bool IsValid(long? phoneNumber)
         {
-            Regex regex = new(@"7(\d{10})");
+            Regex regex = new(@"^(7|8){1}\d{10}$");
             return regex.IsMatch(phoneNumber.ToString());
         }
 
